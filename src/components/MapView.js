3e5c606f63ce513f66e9cb7ecb2b8b76c0a165a6
ni.ho22@student.passerellesnumeriques.org <admin@pnvl085.pn.com.vn>
2022-11-06 23:04:18 +0700

    update gg map

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,12 +1,10 @@
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView,{Marker, PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
  container: {
    ...StyleSheet.absoluteFillObject,
-   height: 400,
-   width: 400,
    justifyContent: 'flex-end',
    alignItems: 'center',
  },
@@ -17,16 +15,23 @@ const styles = StyleSheet.create({
 
 export default () => (
    <View style={styles.container}>
+    
      <MapView
+      zoomControlEnabled={true}
+      zoomEnabled={true}
        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
        style={styles.map}
        region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
+         latitude: 21.0506866,
+         longitude: 105.8047464,
          latitudeDelta: 0.015,
          longitudeDelta: 0.0121,
        }}
-     >
+     ><Marker coordinate={{
+      latitude: 21.0506866,
+      longitude: 105.8047464,}}>
+
+      </Marker>
      </MapView>
    </View>
 );
--- src/navigation/Navigate.js
+++ src/navigation/Navigate.js
@@ -1,20 +1,61 @@
 
 import Profile from '../components/Profile';
 import React from "react";
-import Notify from '../components/Notify';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import StackDetail from './StackNavigate';
 import MapView from '../components/MapView';
+import { Image} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="HomeStack" component={StackDetail} /> 
-            <Tab.Screen name="Profile" component={Profile}/>
-            <Tab.Screen name="Notify" component={Notify}  />
-            <Tab.Screen name="Map" component={MapView}/>
+            <Tab.Screen name="HomeStack" component={StackDetail} 
+            options={{
+                tabBarIcon: ({color, size, focused}) => (
+                    <Image
+                      source={require('../assets/img/homeicon.png')}
+                      style={{
+                        width: 30,
+                        height: 30,
+                        tintColor: '#0d8af0',
+                      }}
+                    />
+                  ),
+            }}
+
+            /> 
+            <Tab.Screen name="Profile" component={Profile}
+                options={{
+                    tabBarIcon: ({color, size, focused}) => (
+                        <Image
+                        source={require('../assets/img/usericon.png')}
+                        style={{
+                            width: 30,
+                            height: 30,
+                            tintColor: '#0d8af0',
+                        }}
+                        />
+                    ),
+                }}
+
+            />
+            <Tab.Screen name="Map" component={MapView}
+                options={{
+                    tabBarIcon: ({color, size, focused}) => (
+                        <Image
+                        source={require('../assets/img/mapicon.png')}
+                        style={{
+                            width: 30,
+                            height: 30,
+                            tintColor: '#0d8af0',
+                        }}
+                        />
+                    ),
+                }}
+            
+            />
         </Tab.Navigator>
     );
 }
